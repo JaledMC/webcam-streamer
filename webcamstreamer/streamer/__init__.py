5408f3ac09c5d46f25f53bd101e20317a793bb17
JaledMC <j.moustafacalvo@gmail.com>
2020-06-22 12:40:19 +0200

    Update __init__.py

--- webcamstreamer/streamer/__init__.py
+++ webcamstreamer/streamer/__init__.py
@@ -19,7 +19,7 @@ from gevent import monkey
 monkey.patch_all()
 
 from flask import Flask, render_template, Response, request
-from flask_caching.socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit
 config = configparser.ConfigParser()
 
 from defaults import defaults
